b3550517f871462a903599087687162279d7d64f
Yuri Brito <yuridebatista@gmail.com>
2023-05-23 19:17:15 -0300

    adicionando cadastro do firebase

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,16 @@
- 
+function auth() {
+    event.preventDefault();
+    const email = document.getElementById('email').value;
+    const senha = document.getElementById('password').value;
+
+    console.log('Email', email);
+
+    firebase.auth().createUserWithEmailAndPassword(email, senha)
+        .then(() => {
+            console.log("Usuario criado com sucesso!!");
+        })
+        .catch((error) => {
+            console.log('Problemas ao criar o usuario!');
+            console.log(error)
+        });
+}
